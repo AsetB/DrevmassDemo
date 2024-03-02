01faaa9a3329471a55d8e9327450cc19081d6bb2
AsetB <asetbakirov@gmail.com>
2024-03-02 23:10:38 +0500

    Initial commit

---
+++ Drevmass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drevmass
+//
+//  Created by Aset Bakirov on 02.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Drevmass
+Application for wood massager manufacturer.
+Internship project.
+
+[App Store](https://apps.apple.com/kz/app/drevmass/id6450933706)
+
+Dependencies:
+ - in-progress
+
