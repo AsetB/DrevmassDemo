2ddc8d12ae3fd12d0e8b58e718d31b6811d9df5a
Aset Bakirov <asetbakirov@gmail.com>
2024-03-05 00:50:29 +0500

    Onboarding added

---
+++ Drevmass/Constants/Onboarding.swift
@@ -0,0 +1,25 @@
+//
+//  Onboarding.swift
+//  Drevmass
+//
+//  Created by Aset Bakirov on 04.03.2024.
+//
+
+import Foundation
+
+struct Onboarding {
+    public static let topLabel1: String = """
+Избавьтесь от боли 
+в спине раз и навсегда!
+"""
+    public static let topLabel2: String = "Наша цель"
+    public static let topLabel3: String = """
+Спина требует 
+ежедневного ухода!
+"""
+    public static let descriptionLabel: String = """
+Здоровье спины – это один
+из основных показателей комфорта
+жизни
+"""
+}
